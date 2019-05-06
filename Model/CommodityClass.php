901140c8fdbd034fe8c8a262792f5e7600f63a45
kakashen <1519176958@qq.com>
2019-05-06 14:23:13 +0800

    添加方法

--- Model/CommodityClass.php
+++ Model/CommodityClass.php
@@ -14,8 +14,32 @@ class CommodityClass extends MysqlDB
         parent::__construct($this->tableName);
     }
 
+    /**
+     * @return array
+     * 根据主键 ccIndex 获取一条数据
+     */
     public function findOne(): array
     {
         return $this->find($this->tableName, [self::ccIndex => $this->id]);
+    }
+
+    /**
+     * @param array $filter
+     * @param array $option
+     * @param string $select
+     * @return array 获取多条数据
+     */
+    private function getMany(array $filter = [], array $option = [], string $select = "*")
+    {
+        $query = $this->select($select)->from($this->tableName)->where($filter);
+
+        if (array_key_exists("order", $option)) {
+            $query = $query->order($option['order']);
+        }
+
+        if (array_key_exists("first", $option)) {
+            return $query->queryRow();
+        }
+        return $query->queryAll();
     }
 }
