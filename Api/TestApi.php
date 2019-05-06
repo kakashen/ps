9b1232cd34e04edcdff033688a76d802756aee07
kakashen <1519176958@qq.com>
2019-05-06 09:01:07 +0800

    添加测试api

--- Api/TestApi.php
+++ Api/TestApi.php
@@ -1,17 +1,42 @@
 <?php
 
 namespace Api;
+
 use API;
 use Request;
+use RequestData;
 use Response;
+use ResponseData;
 
 require_once 'Common.php';
+
+class TestApiRequest extends RequestData
+{
+    public $id;
+}
+
+class TestApiResponse extends ResponseData
+{
+    public $data;
+}
 
 class TestApi extends API
 {
     protected function run(Request $request, Response $response)
     {
-        // TODO: Implement run() method.
-        $response->data = '123';
+        $response->data = new TestApiResponse();
+
+        /**
+         * @var $rqData TestApiRequest
+         */
+        $rqData = $request->data;
+        if (!isset($rqData->id) || !is_int($rqData->id)) {
+            $response->data->code = 10001;
+            $response->data->data = 'id not set or int';
+            return;
+
+        }
+
+        $response->data->data = 123;
     }
 }
